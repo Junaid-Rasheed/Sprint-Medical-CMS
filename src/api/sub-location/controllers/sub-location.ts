70557dcd6b9a61102f7b3934cec8e582f941cb45
Junaid-Rasheed <junaidrasheed300@gmail.com>
2026-02-07 15:58:30 +0500

    sublocation added

---
+++ src/api/sub-location/controllers/sub-location.ts
@@ -0,0 +1,7 @@
+/**
+ * sub-location controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::sub-location.sub-location');
---
+++ src/api/sub-location/services/sub-location.ts
@@ -0,0 +1,7 @@
+/**
+ * sub-location service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sub-location.sub-location');
